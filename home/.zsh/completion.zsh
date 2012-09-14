883a9c001eeee7bea47010128c243302428380cc
pthrasher <philipthrasher@gmail.com>
2012-09-14 13:23:14 -0400

    GOD DAMN YOU

--- home/.zsh/completion.zsh
+++ home/.zsh/completion.zsh
@@ -1,8 +1,12 @@
+autoload -U compinit
+compinit
+
 unsetopt menu_complete   # do not autoselect the first completion entry
 unsetopt flowcontrol
 setopt auto_menu         # show completion menu on succesive tab press
 setopt complete_in_word
 setopt always_to_end
+
 
 zmodload -i zsh/complist
 
