aae66d35923e8ad0d515e83a0591da4212b21ce3
pthrasher <philipthrasher@gmail.com>
2013-01-23 10:57:36 -0500

    No changes after this point.

--- home/.zsh/prompt.zsh
+++ home/.zsh/prompt.zsh
@@ -4,18 +4,19 @@ setopt PROMPT_SUBST
 export VIRTUAL_ENV_DISABLE_PROMPT='yeah'
 export VCPROMPT_FORMAT=" %n:%b%m%u "
 venv_prompt=''
+vcpout=''
 
 PROMPT="    ¬${FG[240]} %c${FX[reset]}: "
-RPROMPT="${FG[240]}$(vcprompt)${venv_prompt}${FX[reset]}"
 
 function prompt-precmd() {
     echo ""
     if [[ -z $VIRTUAL_ENV ]]; then
         venv_prompt=""
     else
-        venv_prompt="[$(basename "$VIRTUAL_ENV")]"
+        venv_prompt=":$(basename "$VIRTUAL_ENV")"
     fi
-    RPROMPT="${FG[247]}$(vcprompt)${venv_prompt}${FX[reset]}"
+    vcpout=`vcprompt`
+    RPROMPT="${FG[247]}${vcpout} ${venv_prompt}${FX[reset]}"
 }
 
 function prompt-preexec() {
