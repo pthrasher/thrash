9df91e18e36c8cbdb350365054900697854dc4d0
pthrasher <philipthrasher@gmail.com>
2013-01-23 10:46:04 -0500

    Gross hack because Andy doesn't know how to code

--- home/.zsh/prompt.zsh
+++ home/.zsh/prompt.zsh
@@ -3,9 +3,15 @@ setopt PROMPT_SUBST
 
 export VIRTUAL_ENV_DISABLE_PROMPT='yeah'
 PROMPT="    ¬${FG[240]} %c${FX[reset]}: "
+RPROMPT="$(vcprompt)${venv_prompt}"
 
 function prompt-precmd() {
     echo ""
+    if [[ -z $VIRTUAL_ENV ]]; then
+        venv_prompt=""
+    else
+        venv_prompt="[$(basename "$VIRTUAL_ENV")]"
+    fi
 }
 
 function prompt-preexec() {
