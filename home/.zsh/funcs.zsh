dd944357622f4575ba090b1005fa2e981858e24f
pthrasher <philipthrasher@gmail.com>
2013-03-04 10:54:11 -0500

    Another commit to keep my CAN streak going.

--- home/.zsh/funcs.zsh
+++ home/.zsh/funcs.zsh
@@ -10,6 +10,17 @@ function groot() {
     cd $(git rev-parse --show-cdup);
 }
 
+# function to execute built-in cd
+function fasd_cd() {
+  if [ \$# -le 1 ]; then
+    fasd "\$@"
+  else
+    local _fasd_ret="\$(fasd -e 'printf %s' "\$@")"
+    [ -z "\$_fasd_ret" ] && return
+    [ -d "\$_fasd_ret" ] && cd "\$_fasd_ret" || printf %s\\n "\$_fasd_ret"
+  fi
+}
+
 function backburn () {
     for i in $@; do
         mv "$i" /Users/pthrasher/Dropbox/code/backBurner/
