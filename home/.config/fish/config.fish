63c497d067600d55343d9099a0ac56d3bf32e3b5
pthrasher <philipthrasher@gmail.com>
2013-04-22 18:56:20 -0400

    ALL SORTS OF THINGS

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,7 +1,7 @@
 function fish_prompt
     set color $fish_color_cwd
     echo -n '    ¬ '
-    echo -n (basename (pwd))
+    echo -n (basename (prompt_pwd))
     set color normal
     echo -n ': '
 end
