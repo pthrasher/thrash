0db306de9d620fa173c0df6787781d1e111b74bb
pthrasher <philipthrasher@gmail.com>
2013-04-22 19:21:14 -0400

    fixed some minor stuff, might need some additional work.

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -20,3 +20,10 @@ function z
     cd (fasd -d -l -r -1 $argv)
 
 end
+
+alias gst=git status
+alias gpo=git pull origin
+alias gpu=git pull upstream
+alias gco=git checkout
+alias gca=git commit -avm
+alias gc=git commit -v
