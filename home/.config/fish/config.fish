0c9be2306c7cbbc228fdab88992cd5e3922cab03
pthrasher <philipthrasher@gmail.com>
2013-05-25 14:43:38 -0400

    derpherp

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -100,6 +100,24 @@ alias gca "git commit -avm"
 alias gc "git commit -v"
 alias gignored "git ls-files -o -i --exclude-standard"
 
+### git checkout aliases completion
+complete -f -c gco -a '(__fish_git_branches)' --description 'Branch'
+complete -f -c gco -a '(__fish_git_tags)' --description 'Tag'
+complete -f -c gco -s b -d 'Create a new branch'
+complete -f -c gco -s t -l track -d 'Track a new branch'
+
+### git pull aliases completion
+complete -f -c gpull -s q -l quiet -d 'Be quiet'
+complete -f -c gpull -s v -l verbose -d 'Be verbose'
+complete -f -c gpull -l all -d 'Fetch all remotes'
+complete -f -c gpull -s a -l append -d 'Append ref names and object names'
+complete -f -c gpull -s f -l force -d 'Force update of local branches'
+complete -f -c gpull -s k -l keep -d 'Keep downloaded pack'
+complete -f -c gpull -l no-tags -d 'Disable automatic tag following'
+complete -f -c gpull -l progress -d 'Force progress status'
+complete -f -c gpull -a '(git remote)' -d 'Remote alias'
+complete -f -c gpull -a '(__fish_git_branches)' -d 'Branch'
+
 alias cdconf 'z sh/ho'
 alias vimrc 'z sh/ho; and v .vimrc'
 alias fishrc 'z sh/ho; and v .config/fish/config.fish'
