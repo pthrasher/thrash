55259cc4df07724bd5f5dc0839fd5fa7365acd5c
pthrasher <philipthrasher@gmail.com>
2013-07-10 11:38:42 -0400

    added aws infos

--- home/.config/fish/env.fish
+++ home/.config/fish/env.fish
@@ -8,6 +8,11 @@ set -x GOPATH ~/Dropbox/code/Go /usr/local/go
 set -x COFFEELINT_CONFIG ~/.coffeelintrc
 set -x EDITOR 'mvim -f'
 
+# export is required for these I think.
+set -x JAVA_HOME (/usr/libexec/java_home)
+set -x AWS_IAM_HOME "/usr/local/opt/aws-iam-tools/jars"
+set -x AWS_CREDENTIAL_FILE $HOME/.aws-credentials-master
+
 set NODE_PATH /usr/local/lib/node_modules $NODE_PATH
 set VCPROMPT_FORMAT '%n:%b%m%u'
 set VIRTUALFISH_COMPAT_ALIASES 'on'
