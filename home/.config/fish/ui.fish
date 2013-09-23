2952b5f42c5908697f61f9cdfa359f4efe87689d
pthrasher <philipthrasher@gmail.com>
2013-09-23 06:24:34 -0400

    tagging release w.t.f.

--- home/.config/fish/ui.fish
+++ home/.config/fish/ui.fish
@@ -1,8 +1,11 @@
 
 function fish_prompt
-    set_color $fish_color_cwd
+    set_color normal
     echo ''
-    echo -n '    ¬ '
+    echo -n '    '
+    set_color $fish_color_cwd
+    echo -n '¬'
+    echo -n ' '
     echo -n (basename (prompt_pwd))
     set_color normal
     echo -n ': '
