6872157142765c520a16abdd1e7221211ce0788a
pthrasher <philipthrasher@gmail.com>
2013-11-25 07:52:30 -0500

    TDD: 1, Me: 0

--- home/.config/fish/ui.fish
+++ home/.config/fish/ui.fish
@@ -10,7 +10,7 @@ function fish_prompt
     echo -n (basename (prompt_pwd))
     set_color $fish_color_cwd
     echo -n ': '
-    echo "\e[00m]"
+    echo -e "\e[00m]"
 end
 
 
@@ -21,7 +21,7 @@ function fish_right_prompt -d "Write out the right prompt"
         set venv (basename "$VIRTUAL_ENV")
         echo -n "venv:$venv"
     end
-    echo "\e[00m]"
+    echo -e "\e[00m]"
 end
 
 
