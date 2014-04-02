b992723ce64c23b4bbee5741c8a0798f138139b3
pthrasher <philipthrasher@gmail.com>
2014-04-02 11:59:20 -0400

    The last time I tried this the monkey didn't survive. Let's hope it works better this time.

--- home/.config/fish/env.fish
+++ home/.config/fish/env.fish
@@ -18,8 +18,8 @@ set -x EDITOR 'mvim -f'
 
 # export is required for these I think.
 set -x JAVA_HOME (/usr/libexec/java_home)
-set -x AWS_IAM_HOME "/usr/local/opt/aws-iam-tools/jars"
-set -x AWS_CREDENTIAL_FILE $HOME/.aws-credentials-master
+# set -x AWS_IAM_HOME "/usr/local/opt/aws-iam-tools/jars"
+# set -x AWS_CREDENTIAL_FILE $HOME/.aws-credentials-master
 
 set NODE_PATH /usr/local/lib/node_modules $NODE_PATH
 set VCPROMPT_FORMAT '%n:%b%m%u'
